2821afc839f5a49f5527cf55eaacc86e4035a9e1
Omar Faruk <programmer.omar.dev@gmail.com>
2022-05-30 12:32:54 +0600

    Add save add button

--- pages/components/header.tsx
+++ pages/components/header.tsx
@@ -6,7 +6,7 @@ import { CaretDownOutlined } from "@ant-design/icons";
 const { Header: AntHeader } = Layout;
 const Header: FC = () => {
   return (
-    <AntHeader className="bg-white px-2 border-b flex items-center justify-between">
+    <AntHeader className="bg-white px-2 border-b flex items-center justify-between md:px-10 lg:px-32">
       <div className="logo" />
       <Menu
         className={`relative w-full md:h-full md:py-0`}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
-import { Layout } from "antd";
+import { Button, Layout } from "antd";
 import { useState } from "react";
-import { TeamOutlined } from "@ant-design/icons";
+import { PlusOutlined, TeamOutlined } from "@ant-design/icons";
 import Header from "./components/header";
 import Input from "./components/Input";
 import Select from "./components/Select";
@@ -10,21 +10,45 @@ import { Countries } from "../constants";
 const Home: NextPage = () => {
   return (
     <Layout className="w-full">
+      <Header />
       <div className="bg-white px-5 md:px-10 lg:px-32">
-        <Header />
-        <div className="flex mt-10 items-center gap-2 text-3xl ">
-          <TeamOutlined className="text-blue-500" />
-          <span>
-            <h2 className="font-semibold">New Team</h2>
-          </span>
+        <div>
+          <div className="flex mt-10 items-center gap-2 text-3xl ">
+            <TeamOutlined className="text-blue-500" />
+            <span>
+              <h2 className="font-semibold">New Team</h2>
+            </span>
+          </div>
+          <div className="py-5 grid grid-cols-1 md:grid-cols-2   gap-x-10 gap-y-5">
+            <Input label="Team name" />
+            <Input label="Primary Email" />
+            <Input label="Company Name" />
+            <Select data={Countries} label="Country" />
+            <Input label="Password" type="password" />
+            <Input label="Repeat Password" type="password" />
+          </div>
+          <div className="flex items-center justify-end">
+            <Button
+              className="flex items-center gap-2 rounded-md !ml-auto !bg-gradient-to-br from-lime-400 to-green-500 text-white  hover:!from-green-500 hover:text-white border-0 transition-all duration-300"
+              size="large"
+            >
+              <span>
+                <PlusOutlined
+                  className="!inline-block !mb-2"
+                  style={{ marginBottom: 20 }}
+                />
+              </span>
+              <span>Add</span>
+            </Button>
+          </div>
         </div>
-        <div className="py-5 grid grid-cols-1 md:grid-cols-2   gap-x-10 gap-y-5">
-          <Input label="Team name" />
-          <Input label="Primary Email" />
-          <Input label="Company Name" />
-          <Select data={Countries} label="Country" />
-          <Input label="Password" type="password" />
-          <Input label="Repeat Password" type="password" />
+        <div className="">
+          <div>
+            <h2 className="font-semibold text-2xl">Member</h2>
+            <span className="flex items-center justify-center w-5 h-5  bg-blue-500 rounded-full text-white text-sm">
+              3
+            </span>
+          </div>
         </div>
       </div>
     </Layout>
